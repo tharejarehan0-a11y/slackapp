031b61783b7cde9dfe2f89763463d0b290ced649
Rehan-thareja <tharejarehan0@gmail.com>
2026-07-16 17:17:38 +0530

    intialised app

---
+++ app.py
@@ -0,0 +1,20 @@
+import os 
+
+from dotenv import load_dotenv
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+load_dotenv()
+
+app = App(
+    token=os.environ["SLACK_BOT_TOKEN"],
+    signing_secret=os.environ["SLACK_SIGNING_SECRET"],
+)
+#commands
+
+
+if __name__ == "__main__" :
+    SocketModeHandler(
+        app,
+        os.environ["SLACK_APP_TOKEN"]
+    ).start()
